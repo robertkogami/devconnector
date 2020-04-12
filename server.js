a362227bc48484b024834e93a85dd1b673e77a3c
Robert Kogami <robert@kogami.dev>
2020-04-12 11:50:59 -0400

    Added DB configs and connected to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB(); // Connect to database
 
 app.get("/", (req, res) => res.send("API Running"));
 
