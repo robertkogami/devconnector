9010330095042aa950620b7cbcb80cb93eb16c1a
Robert Kogami <robert@kogami.dev>
2020-04-12 23:55:38 -0400

    completed login and register forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log('SUCCESS');
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Login</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign into Your Account
+      </p>
+      <form onSubmit={e => onSubmit(e)} className='form'>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            minLength='6'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type='submit' value='Login' className='btn btn-primary' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
